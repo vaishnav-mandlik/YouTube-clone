0da2cc47a066a8501c5c1e29d4a0e0864af5ad00
Vaishnav Mandlik <vaishnavmandlik@yahoo.com>
2022-09-21 22:57:58 +0530

    Add Video

--- search.js
+++ search.js
@@ -2,16 +2,18 @@ let videos = document.getElementById("videos");
 
 let getData = async (inp) => {
   let data = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${inp}&key=AIzaSyCp_fGmjqW95H6tdQ-R2_ywrh941RbFkms`
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${inp}&key=AIzaSyCp_fGmjqW95H6tdQ-R2_ywrh941RbFkms`
   );
   let d1 = await data.json();
   d1 = d1.items;
   console.log(d1);
   videos.innerHTML = "";
 
-  d1.forEach((el) => {
-    // const {snippet.channelTitle,} = el;
-    let videoHTML = ` <div class="flex flex-row mt-5 ml-[5%] mr-[10%] w-[full] h-[200px]">
+  d1.forEach(function (el) {
+    let vid = String(el.id.videoId);
+    // console.log(vid);
+    // console.log(typeof vid);
+    videos.innerHTML += ` <div  class="flex flex-row mt-5 ml-[5%] mr-[10%] w-[full] h-[200px] hover:cursor-pointer"  onclick="getVdoId()" id=${vid}  >
     <img
       src=${el.snippet.thumbnails.medium.url}
       alt=""
@@ -23,11 +25,12 @@ let getData = async (inp) => {
     
     </div>
   </div>`;
-    videos.innerHTML += videoHTML;
   });
 };
 function showVdo() {
   let inp = document.getElementById("inp").value;
+  console.log("clicked");
+  localStorage.setItem("inp", JSON.stringify(inp));
   getData(inp);
 }
 
@@ -37,6 +40,18 @@ input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
     let inp = document.getElementById("inp").value;
+    localStorage.setItem("inp", JSON.stringify(inp));
+
     getData(inp);
   }
 });
+
+let vdoId = JSON.parse(localStorage.getItem("inp"));
+if (vdoId) {
+  getData(vdoId);
+}
+function getVdoId() {
+  let inp = event.target.parentNode.id;
+  localStorage.setItem("vdeID", JSON.stringify(inp));
+  location.href = "video.html";
+}
